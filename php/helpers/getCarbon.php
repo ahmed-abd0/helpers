a7aeb568e3bd13b9164fd93e556521820fbd6e10
Ahmed123567 <89392661+Ahmed123567@users.noreply.github.com>
2023-05-30 01:12:44 +0300

    Update getCarbon triple equal

--- php/helpers/getCarbon.php
+++ php/helpers/getCarbon.php
@@ -6,7 +6,7 @@ if(!function_exists("getCarbon")) {
         function getCarbon(...$dates) : Carbon | array {
             $carbon = [];
 
-            if(count($dates) == 1) {
+            if(count($dates) === 1) {
                 return $dates[0] instanceof Carbon ? $dates[0] : Carbon::parse($dates[0]);
             }
 
