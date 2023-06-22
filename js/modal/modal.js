61101ab258f80ad45cf5702f1a42e72e40bd3188
Ahmed123567 <amabdo00001@gmail.com>
2023-06-22 10:21:48 +0300

    modal view

--- js/modal/modal.js
+++ js/modal/modal.js
@@ -28,3 +28,26 @@ $(".modal_btn").on("click", function() {
         $("#global_modal").modal()
     })
 })
+
+`
+<div class="modal-body">
+
+    <form class="validate" action="{{ route('consultant.store') }}" method="post" enctype="multipart/form-data">
+
+        @csrf
+       
+            <div class="modal-footer">
+                <button type="button" data-bs-dismiss='modal'
+                    class="btn btn-primary main-btn">{{ __('message.dismiss') }}</button>
+                <button type="submit" id="BarcodeSubmit" class="btn btn-primary main-btn">{{ __("labels.save_changes") }}</button>
+            </div>
+
+        </div>
+
+
+    </form>
+
+
+</div>
+
+`
